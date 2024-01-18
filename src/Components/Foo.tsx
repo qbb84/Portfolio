c97eeb4dacc3762aaf63e33555c4507a953a94a2
luke <luke.wilder2000@gmail.com>
2024-01-18 11:10:53 +0000

    Scroll
    Implemented the basis for scroll

---
+++ src/Components/Foo.tsx
@@ -0,0 +1,13 @@
+import {useRef} from "react";
+
+export default function Foo(props) {
+    const ref = useRef();
+    return (
+        <>
+            <mesh ref={ref} {...props}>
+                <boxGeometry/>
+                <meshToonMaterial/>
+            </mesh>
+        </>
+    );
+}
