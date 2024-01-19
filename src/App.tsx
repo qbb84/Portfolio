da8cb8a03700da5e0eadc97ca15c774218aca164
luke <luke.wilder2000@gmail.com>
2024-01-19 00:10:32 +0000

    Add new sections and dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,29 @@
 ﻿import "./assets/Styles/App.css";
-import {Canvas} from "@react-three/fiber";
+import { Canvas } from "@react-three/fiber";
 import ScrollAnimation from "./Components/ScrollAnimation";
 import Debug from "./Components/Test/Debug";
 import OrbitControl from "./Components/Test/OrbitControl";
+import CreateButton from "./Components/Hud/CreateButton";
 
 function App() {
-    return (
-        <>
-            <Canvas
-                flat
-                gl={{antialias: false}}
-                camera={{
-                    fov: 50,
-                    near: 0.1,
-                    position: [3, 2, 6],
-                    
-                
-                }}
-            >
-                <Debug/>
-                <OrbitControl/>
-                <ScrollAnimation/>
-            </Canvas>
-        </>
-    );
+  return (
+    <>
+      <CreateButton isStatic={true} />
+      <Canvas
+        flat
+        gl={{ antialias: false }}
+        camera={{
+          fov: 50,
+          near: 0.1,
+          position: [3, 2, 6],
+        }}
+      >
+        <Debug />
+        <OrbitControl />
+        <ScrollAnimation />
+      </Canvas>
+    </>
+  );
 }
 
 export default App;
