acc5d824091521724320b3d3a35d1035782509c6
luke <luke.wilder2000@gmail.com>
2024-01-25 22:25:25 +0000

    Loading done
    Fixed loading screen

--- src/LoadingScreen/Loading.tsx
+++ src/LoadingScreen/Loading.tsx
@@ -2,20 +2,29 @@ import {
   CameraShake,
   Float,
   Html,
-  MeshPortalMaterial,
+  MeshRefractionMaterial,
   useProgress,
 } from '@react-three/drei';
-import { Suspense, useEffect, useRef, useState } from 'react';
-import { Room } from '../Components/ModelRender/Room';
+import { Suspense, useEffect, useState } from 'react';
+import { Room, useRoomGLTF } from '../Components/ModelRender/Room';
 import Debug from '../Components/Test/Debug';
 import Intro from './Intro';
-import Logo, { Loading2 } from './Logo';
-import React from 'react';
-import { BoxGeometry } from 'three';
+import { TextureLoader, WebGLCubeRenderTarget } from 'three';
+import { useThree } from '@react-three/fiber';
 
-function Load() {
+function Load({ handlePlayClick }) {
   const { progress } = useProgress();
   const [displayedProgress, setDisplayedProgress] = useState(0);
+  const [showPlayButton, setShowPlayButton] = useState(false);
+
+  useEffect(() => {
+    const playButtonTimeoutId = setTimeout(() => {
+      setShowPlayButton(true);
+    }, 6300);
+    return () => {
+      clearTimeout(playButtonTimeoutId);
+    };
+  }, []);
 
   useEffect(() => {
     let animationFrameId: number;
@@ -36,39 +45,32 @@ function Load() {
 
   return (
     <>
-      <LoadingSleepAnimation progress={displayedProgress.toFixed(2)} />
-
-      <Float>
-        {/* <Logo />
-        //TODO fade animations for button
-        <Html center>
-          <div className="loading-text-container">
-            <p className="loading-text animate__animated animate__zoomInDown">
-              Taking a snooze... {displayedProgress.toFixed(2)}%
-            </p>
-          </div>
-        </Html> */}
-      </Float>
+      <LoadingSleepAnimation
+        showPlayButton={showPlayButton}
+        handlePlayClick={handlePlayClick}
+      />
     </>
   );
 }
 
 export default function Loading() {
   const [showMainContent, setShowMainContent] = useState(false);
-  const [showPlayButton, setShowPlayButton] = useState(false);
   const [isClicked, setIsClicked] = useState(false);
+  const [showBlink, setShowBlink] = useState(false); // Add this line
 
   useEffect(() => {
-    const playButtonTimeoutId = setTimeout(() => {
-      setShowPlayButton(true);
-    }, 6300);
+    const blinkTimeout = setTimeout(() => {
+      setShowBlink(true); // Update this line
+    }, 29000);
+    return () => clearTimeout(blinkTimeout);
+  }, []);
 
+  useEffect(() => {
     const timeoutId = setTimeout(() => {
       setShowMainContent(true);
-    }, 180000);
+    }, 30100);
 
     return () => {
-      clearTimeout(playButtonTimeoutId);
       clearTimeout(timeoutId);
     };
   }, []);
@@ -82,32 +84,15 @@ export default function Loading() {
   };
 
   return (
-    <Suspense fallback={<Load />}>
+    <Suspense fallback={<Load handlePlayClick={undefined} />}>
       {!showMainContent && (
         <>
-          <Load />
-          <Html center>
-            {showPlayButton && (
-              <button
-                className="button"
-                data-text="Awesome"
-                onClick={handlePlayClick}
-              >
-                <span className="actual-text">&nbsp;wake up&nbsp;</span>
-                <span aria-hidden="true" className="hover-text">
-                  &nbsp;wake up&nbsp;
-                </span>
-              </button>
-            )}
-          </Html>
+          {showBlink && <Blink />}
+          <Load handlePlayClick={handlePlayClick} />
+
           {isClicked && (
             <>
-              <Html position={[0, 21, 0]}>
-                <div className="eyes-shutting-top-click"></div>
-              </Html>
-              <Html position={[0, -61, 0]}>
-                <div className="eyes-shutting-bottom-click"></div>
-              </Html>
+              <Blink />
             </>
           )}
         </>
@@ -124,7 +109,7 @@ export default function Loading() {
 }
 
 /* */
-export function LoadingSleepAnimation({ progress }) {
+export function LoadingSleepAnimation({ showPlayButton, handlePlayClick }) {
   return (
     <>
       <CameraShake intensity={0.1} />
@@ -132,14 +117,30 @@ export function LoadingSleepAnimation({ progress }) {
       <Html position={[0, 21, 0]}>
         <div className="eyes-shutting-top"></div>
       </Html>
-      <Html position={[0, -61, 0]}>
+      <Html position={[0, -61, 0]} zIndexRange={[50000000, 5000000]}>
         <div className="eyes-shutting-bottom"></div>
       </Html>
       <Float>
-        <Html zIndexRange={[5]} onPointerOver={() => console.log('Hovered')}>
+        <Html zIndexRange={5} onPointerOver={() => console.log('Hovered')}>
           <div className="eye">
             <div className="loader">
-              <div className="intern"></div>
+              <div className="intern">
+                <div className="load-percent"></div>
+
+                {showPlayButton && (
+                  // <NewLoad handlePlayClick={handlePlayClick} />
+                  <button
+                    className="button"
+                    data-text="Awesome"
+                    onClick={handlePlayClick}
+                  >
+                    <span className="actual-text">&nbsp;wake up&nbsp;</span>
+                    <span aria-hidden="true" className="hover-text">
+                      &nbsp;wake up&nbsp;
+                    </span>
+                  </button>
+                )}
+              </div>
               <div className="external-shadow">
                 <div className="central"></div>
               </div>
@@ -147,6 +148,62 @@ export function LoadingSleepAnimation({ progress }) {
           </div>
         </Html>
       </Float>
+    </>
+  );
+}
+
+function Blink() {
+  return (
+    <>
+      <Html position={[0, 21, 0]}>
+        <div className="eyes-shutting-top-click"></div>
+      </Html>
+      <Html position={[0, -61, 0]}>
+        <div className="eyes-shutting-bottom-click"></div>
+      </Html>
+    </>
+  );
+}
+
+function NewLoad({ handlePlayClick }) {
+  return (
+    <>
+      <a href="#" className="link" onClick={handlePlayClick}>
+        <svg
+          viewBox="0 0 200 200"
+          width="200"
+          height="200"
+          xmlns="http://www.w3.org/2000/svg"
+          className="link__svg"
+          aria-labelledby="link1-title link1-desc"
+        >
+          <title id="link1-title">Come quick and click this</title>
+          <desc id="link1-desc">
+            A rotating link with text placed around a circle with an arrow
+            inside
+          </desc>
+
+          <path
+            id="link-circle"
+            className="link__path"
+            d="M 20, 100 a 80,80 0 1,1 160,0 a 80,80 0 1,1 -160,0"
+            stroke="none"
+            fill="none"
+          />
+
+          <path
+            className="link__arrow"
+            d="M 75 100 L 125 100 L 110 85 M 125 100 L 110 115"
+            fill="none"
+          />
+
+          <text className="link__text">
+            <textPath href="#link-circle" stroke="none">
+              Click Me To Wake Up
+            </textPath>
+          </text>
+        </svg>
+      </a>
     </>
   );
 }
