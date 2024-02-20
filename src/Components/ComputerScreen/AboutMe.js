70c49894edadcf9029e79af21e5ee7f1200cd4cf
qbb84 <luke.wilder2000@gmail.com>
2024-02-20 17:10:54 +0000

    Implemented desktop icons

--- src/Components/ComputerScreen/AboutMe.js
+++ src/Components/ComputerScreen/AboutMe.js
@@ -1,10 +1,14 @@
-var button = document.getElementsByClassName('Portfolio')[0];
-var div = document.getElementsByClassName('portfolioClicked')[0];
+document.addEventListener('DOMContentLoaded', function () {
+  var button = document.getElementsByClassName('Portfolio')[0];
+  var div = document.getElementsByClassName('portfolioClicked')[0];
 
-button.addEventListener('click', function () {
-  if (div.style.display === 'none' || div.style.display === '') {
+  var exitButton = document.getElementsByClassName('Exit')[0];
+
+  button.addEventListener('click', function () {
     div.style.display = 'block';
-  } else {
+  });
+
+  exitButton.addEventListener('click', function () {
     div.style.display = 'none';
-  }
+  });
 });
