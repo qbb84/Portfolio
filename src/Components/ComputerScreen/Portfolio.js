c43429888aea81672e0f34c2a5581a53d70529d0
qbb84 <luke.wilder2000@gmail.com>
2024-02-22 16:13:48 +0000

    added working about me desktop icon

--- src/Components/ComputerScreen/Portfolio.js
+++ src/Components/ComputerScreen/Portfolio.js
@@ -1,8 +1,8 @@
 document.addEventListener('DOMContentLoaded', function () {
-  var button = document.getElementsByClassName('Portfolio')[0];
-  var div = document.getElementsByClassName('portfolioClicked')[0];
+  var button = document.getElementsByClassName('About')[0];
+  var div = document.getElementsByClassName('aboutClicked')[0];
 
-  var exitButton = document.getElementsByClassName('portfolioExit')[0];
+  var exitButton = document.getElementsByClassName('aboutExit')[0];
 
   button.addEventListener('click', function () {
     div.style.display = 'block';
